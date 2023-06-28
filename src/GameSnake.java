b761e7f2a33011f6a69ec059904ffafbb330b9de
MrSergeyAtWork <forex187000@gmail.com>
2023-06-28 14:51:16 +0300

    Snake 1.1

--- src/GameSnake.java
+++ src/GameSnake.java
@@ -12,37 +12,30 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class GameSnake {
 
-    final String TITLE_OF_PROGRAM = "Game Snake";
-    final String GAME_OVER_MSG = "GAME OVER";
-    final int POINT_RADIUS = 20; // in pix
-    final int FILLED_WIDTH = 20; //in point
-    final int FILLED_HEIGHT = 20; // in point
-    final int START_LOCATION = 300;
+    final int POINT_RADIUS = 20;
+    final int FIELD_WIDTH = 20;
+    final int FIELD_HEIGHT = 20;
     final int START_SNAKE_SIZE = 6;
     final int START_SNAKE_X = 10;
     final int START_SNAKE_Y = 10;
-    final int SHOW_DEALAY = 150;
+    final int DEALAY = 150;
     final int LEFT = 37;
     final int UP = 38;
     final int RIGHT = 39;
     final int DOWN = 40;
     final int PAUSE = 32;
     final int MUTE = 77;
-    final int START_DIRECTION = PAUSE;
-    final Color SNAKE_COLOR = Color.green;
-    final Color FOOD_COLOR = Color.RED;
     Snake snake;
     Food food;
-    JFrame frame;
-    JFrame frameSt;
-    Canvas canvasPanel;
-    Start start;
-    Random random = new Random();
+    GameField gameField;
+    StartWindow startWindow;
+    Island island;
+    JFrame frame, frameSt;
+    Clip clip;
     boolean gameOver = false;
     boolean var = false;
     boolean mute = false;
-    Island island;
-    Clip clip;
+    Random random = new Random();
 
 
     ///////////////////////////////////////////MAIN////////////////////////////////////////////////////////////
@@ -60,18 +53,18 @@ public class GameSnake {
     //Метод игры
     void go() {
         //Создание окно игры
-        frame = new JFrame(TITLE_OF_PROGRAM + " : " + START_SNAKE_SIZE);
+        frame = new JFrame("Game Snake" + " : " + START_SNAKE_SIZE);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setSize(FILLED_WIDTH * POINT_RADIUS + 14, FILLED_HEIGHT * POINT_RADIUS + 37);
-        frame.setLocation(START_LOCATION, START_LOCATION);
+        frame.setSize(FIELD_WIDTH * POINT_RADIUS + 14, FIELD_HEIGHT * POINT_RADIUS + 37);
+        frame.setLocation(300, 300);
         frame.setResizable(false);
         frame.toBack();
         //Создаем дизайн в окне игры
-        canvasPanel = new Canvas();
-        canvasPanel.setBackground(Color.gray);
+        gameField = new GameField();
+        gameField.setBackground(Color.gray);
         //Центрирование
-        frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, gameField);
         //Прослушивать кнопок
         frame.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
@@ -82,9 +75,10 @@ public class GameSnake {
                 }
             }
         });
-        snake = new Snake(START_SNAKE_X, START_SNAKE_Y, START_SNAKE_SIZE, START_DIRECTION); //Создаем новую змейку
+        snake = new Snake(START_SNAKE_X, START_SNAKE_Y, START_SNAKE_SIZE, PAUSE); //Создаем новую змейку
         food = new Food(); //Создаем новую точку с едой
         island = new Island(); //Создаем новый остров
+        island.newIsland();
         island.newIsland();
         island.newIsland();
         island.newIsland();
@@ -95,15 +89,18 @@ public class GameSnake {
             snake.move(); //Змейка идет
             if (food.isEaten()) food.next();  //Если точка съедена - Создать новую точку
 
-            canvasPanel.repaint(); //Перекрасить в зеленый новую точку в змейке
+            gameField.repaint(); //Перекрасить в зеленый новую точку в змейке
 
             try {
-                Thread.sleep(SHOW_DEALAY); //Задержка движения змейки
+                Thread.sleep(DEALAY); //Задержка движения змейки
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
+    ///////////////////////////////////////////////////START//////////////////////////////////////////////////////
 
 
     //Метод окна старт
@@ -112,15 +109,15 @@ public class GameSnake {
         frameSt = new JFrame("Game SNAKE");
         frameSt.setVisible(true);
         frameSt.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frameSt.setSize(FILLED_WIDTH * POINT_RADIUS + 13, FILLED_HEIGHT * POINT_RADIUS + 36);
+        frameSt.setSize(FIELD_WIDTH * POINT_RADIUS + 13, FIELD_HEIGHT * POINT_RADIUS + 36);
         frameSt.setResizable(false);
-        frameSt.setLocation(START_LOCATION, START_LOCATION);
+        frameSt.setLocation(300, 300);
         frameSt.toFront();
         //Создаем дизайн в окне игры
-        start = new Start();
-        start.setBackground(Color.gray);
+        startWindow = new StartWindow();
+        startWindow.setBackground(Color.gray);
 
-        frameSt.getContentPane().add(BorderLayout.CENTER, start);
+        frameSt.getContentPane().add(BorderLayout.CENTER, startWindow);
     }
 
 
@@ -176,17 +173,17 @@ public class GameSnake {
                 y++;
             }
             //Перенос змейки чтоб она не упиралась в границы окна
-            if (x > FILLED_WIDTH - 1) {
+            if (x > FIELD_WIDTH - 1) {
                 x = 0;
             }
             if (x < 0) {
-                x = FILLED_WIDTH - 1;
+                x = FIELD_WIDTH - 1;
             }
-            if (y > FILLED_WIDTH - 1) {
+            if (y > FIELD_WIDTH - 1) {
                 y = 0;
             }
             if (y < 0) {
-                y = FILLED_HEIGHT - 1;
+                y = FIELD_HEIGHT - 1;
             }
             //Конец игры, проверка метода
             if (isInsideSnake(x, y) || island.isInsideIsland(x, y))
@@ -197,7 +194,7 @@ public class GameSnake {
 
             if (isFood(food)) { //Если точка это еда?
                 food.eat(); // Едим
-                frame.setTitle(TITLE_OF_PROGRAM + " : " + snake.size()); //Меняем длину змейки в заголовке
+                frame.setTitle("Game Snake" + " : " + snake.size()); //Меняем длину змейки в заголовке
             } else {
                 snake.remove(snake.size() - 1); //Либо уменьшаем длину змейки
             }
@@ -260,7 +257,7 @@ public class GameSnake {
         //Конструктор
         public Food() {
             super(-1, -1);
-            this.color = FOOD_COLOR;
+            this.color = Color.RED;
         }
 
         //Метод съесть точку
@@ -275,8 +272,8 @@ public class GameSnake {
 
         Point random(){
             int x, y;
-            x = random.nextInt(FILLED_WIDTH);
-            y = random.nextInt(FILLED_HEIGHT);
+            x = random.nextInt(FIELD_WIDTH);
+            y = random.nextInt(FIELD_HEIGHT);
             if (island.isInsideIsland(x, y) || snake.isInsideSnake(x, y))
                 random();
             return new Point(x,y);
@@ -314,8 +311,8 @@ public class GameSnake {
         void newIsland() {
             int x, y;
             do {
-                x = random.nextInt(FILLED_WIDTH - 3);
-                y = random.nextInt(FILLED_HEIGHT - 3);
+                x = random.nextInt(FIELD_WIDTH - 3);
+                y = random.nextInt(FIELD_HEIGHT - 3);
             } while (snake.isInsideSnake(x, y) || isInsideIsland(x, y));
             list(x, y);
         }
@@ -344,7 +341,7 @@ public class GameSnake {
 
     class Point {
         int x, y;
-        Color color = SNAKE_COLOR;
+        Color color = Color.green;
 
         //Конструктор
         public Point(int x, int y) {
@@ -378,7 +375,7 @@ public class GameSnake {
     /////////////////////////////////////////////ОКНО ИГРЫ////////////////////////////////////////////////
 
 
-    public class Canvas extends JPanel {
+    public class GameField extends JPanel {
 
         @Override
         public void paint(Graphics q) {
@@ -399,16 +396,16 @@ public class GameSnake {
                 q.setColor(Color.red);
                 q.setFont(new Font("Arial", Font.BOLD, 38));
                 FontMetrics fm = q.getFontMetrics();
-                q.drawString(GAME_OVER_MSG, (FILLED_WIDTH * POINT_RADIUS - fm.stringWidth(GAME_OVER_MSG)) / 2,
-                        (FILLED_HEIGHT * POINT_RADIUS) / 2);
+                q.drawString("GAME OVER", (FIELD_WIDTH * POINT_RADIUS - fm.stringWidth("GAME OVER")) / 2,
+                        (FIELD_HEIGHT * POINT_RADIUS) / 2);
             }
             //Надпись ПАУЗА
             if (var) {
                 q.setColor(Color.red);
                 q.setFont(new Font("Arial", Font.BOLD, 48));
                 FontMetrics p = q.getFontMetrics();
-                q.drawString("PAUSE", (FILLED_WIDTH * POINT_RADIUS - p.stringWidth("PAUSE")) / 2,
-                        (FILLED_HEIGHT * POINT_RADIUS) / 2);
+                q.drawString("PAUSE", (FIELD_WIDTH * POINT_RADIUS - p.stringWidth("PAUSE")) / 2,
+                        (FIELD_HEIGHT * POINT_RADIUS) / 2);
             }
         }
     }
@@ -417,7 +414,7 @@ public class GameSnake {
     /////////////////////////////////////////////ОКНО СТАРТ////////////////////////////////////////////////
 
 
-    public class Start extends JPanel {
+    public class StartWindow extends JPanel {
 
         @Override
         public void paint(Graphics q) {
@@ -431,16 +428,16 @@ public class GameSnake {
             q.setColor(Color.green);
             q.setFont(new Font("Arial", Font.BOLD, 50));
             FontMetrics start = q.getFontMetrics();
-            q.drawString("SNAKE", (FILLED_WIDTH * POINT_RADIUS - start.stringWidth("SNAKE")) / 2,
-                    (FILLED_HEIGHT * POINT_RADIUS) / 2);
+            q.drawString("SNAKE", (FIELD_WIDTH * POINT_RADIUS - start.stringWidth("SNAKE")) / 2,
+                    (FIELD_HEIGHT * POINT_RADIUS) / 2);
             q.setColor(Color.red);
             q.setFont(new Font("Arial", Font.BOLD, 22));
             FontMetrics rules = q.getFontMetrics();
-            q.drawString("SPASE - Pause", (FILLED_WIDTH * POINT_RADIUS - rules.stringWidth("SPASE - Pause")) / 2,
+            q.drawString("SPASE - Pause", (FIELD_WIDTH * POINT_RADIUS - rules.stringWidth("SPASE - Pause")) / 2,
                     310);
             q.setFont(new Font("Arial", Font.BOLD, 22));
             FontMetrics rules2 = q.getFontMetrics();
-            q.drawString("M - Mute sound", (FILLED_WIDTH * POINT_RADIUS - rules2.stringWidth("M - Mute sound")) / 2,
+            q.drawString("M - Mute sound", (FIELD_WIDTH * POINT_RADIUS - rules2.stringWidth("M - Mute sound")) / 2,
                     350);
             //Добавляем прослушивать кнопки
             button.addActionListener(new ActionListener() {
